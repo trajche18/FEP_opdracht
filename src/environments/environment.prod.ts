edcbf269613923ff89e7a45d7a99140de22cbefd
Mohamed Aarab <mohamed_aarab@live.nl>
2017-11-01 17:15:54 +0100

    package changes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyBWC40IsmiLzDDawMbZK4kjKG9mkh5kJOE",
+        authDomain: "hardware-lenen.firebaseapp.com",
+        databaseURL: "https://hardware-lenen.firebaseio.com",
+        projectId: "hardware-lenen",
+        storageBucket: "hardware-lenen.appspot.com",
+        messagingSenderId: "740504731528"
+    }
+};
